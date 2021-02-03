1a71f9516410670f88a946404075f7ca095f7523
Ashley Wright <ahmedtrg981106@gmail.com>
2021-02-03 06:17:17 +0300

    Remove functional stuff cause captured variables bad

--- src/main/java/mod/coda/fins/item/SwampDidgeridooItem.java
+++ src/main/java/mod/coda/fins/item/SwampDidgeridooItem.java
@@ -1,5 +1,6 @@
 package mod.coda.fins.item;
 
+import mod.coda.fins.entity.MudhorseEntity;
 import mod.coda.fins.init.FinsEntities;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUseContext;
@@ -13,7 +14,9 @@ public class SwampDidgeridooItem extends Item {
     @Override
     public ActionResultType onItemUse(ItemUseContext context) {
         if (context.getPlayer() != null) {
-            context.getWorld().getEntitiesWithinAABB(FinsEntities.MUDHORSE.get(), context.getPlayer().getBoundingBox().grow(8), entity -> entity.getCommander() == null).forEach(entity -> entity.setCommander(context.getPlayer()));
+            for (MudhorseEntity mudhorseEntity : context.getWorld().getEntitiesWithinAABB(FinsEntities.MUDHORSE.get(), context.getPlayer().getBoundingBox().grow(8), entity -> entity.getCommander() == null)) {
+                mudhorseEntity.setCommander(context.getPlayer());
+            }
             context.getPlayer().getCooldownTracker().setCooldown(this, 1200);
         }
         return super.onItemUse(context);
