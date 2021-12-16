3e4c158ac3226b1bb4e3ef77f76267358623f942
Coda <cody.koelliker@outlook.com>
2021-12-16 11:40:38 -0700

    sort of

--- src/main/java/teamdraco/fins/common/world/tree/LaminaTreeFeature.java
+++ src/main/java/teamdraco/fins/common/world/tree/LaminaTreeFeature.java
@@ -16,90 +16,51 @@ import java.util.Random;
 
 public class LaminaTreeFeature extends Feature<ProbabilityConfig> {
     private static final BlockState STALK = FinsBlocks.LAMINA_STALK.get().defaultBlockState().setValue(RotatedPillarBlock.AXIS, Direction.Axis.Y);
-    private static final BlockState PADS = FinsBlocks.LAMINA_PADS.get().defaultBlockState().setValue(SlabBlock.WATERLOGGED, true);
+    private static final BlockState WATERLOGGED_PADS = FinsBlocks.LAMINA_PADS.get().defaultBlockState().setValue(SlabBlock.WATERLOGGED, true);
+    private static final BlockState PADS = FinsBlocks.LAMINA_PADS.get().defaultBlockState();
 
     public static final Direction[] DIRECTIONS = new Direction[]{Direction.WEST, Direction.NORTH, Direction.SOUTH, Direction.EAST};
 
     //trunk placement
-    public static int minimumTrunkHeight = 4;
-    public static int trunkHeightExtra = 1;
-
-    //branches on the trunk placement
-    public static int minimumBranchHeight = 2;
-    public static int branchHeightExtra = 0;
+    public static int minimumTrunkHeight = 6;
+    public static int trunkHeightExtra = 2;
 
     public LaminaTreeFeature(Codec<ProbabilityConfig> codec) {
         super(codec);
     }
 
     @Override
-    public boolean place(ISeedReader iSeedReader, ChunkGenerator chunkGenerator, Random random, BlockPos blockPos, ProbabilityConfig config) {
+    public boolean place(ISeedReader reader, ChunkGenerator chunkGenerator, Random random, BlockPos blockPos, ProbabilityConfig config) {
         ArrayList<Entry> filler = new ArrayList<>();
         ArrayList<Entry> leavesFiller = new ArrayList<>();
         int trunkHeight = minimumTrunkHeight + random.nextInt(trunkHeightExtra + 1);
         for (int i = 0; i < trunkHeight; i++) {
-            for (int j = 0; j < 1; j++) {
-                BlockPos trunkPos = blockPos.offset(j, i, j);
-                if (i == 0 && !canGrowTree(iSeedReader, trunkPos)) {
+            for (int j = 0; j < 4; j++) {
+                int xOffset = j % 2;
+                int zOffset = j / 2;
+                BlockPos trunkPos = blockPos.offset(xOffset, i, zOffset);
+                if (i == 0 && !canGrowTree(reader, trunkPos)) {
                     return false;
                 }
-                boolean success = filler.add(new Entry(trunkPos, STALK));;
+                boolean success = filler.add(new Entry(trunkPos, STALK));
                 if (!success) {
                     return false;
                 }
 
-                int leavesHeight = trunkHeight / 2;
+                int leavesHeight = trunkHeight;
 
-                for (Direction direction : DIRECTIONS) {
-                    BlockPos leavesPos = blockPos.offset(j, leavesHeight, j).relative(direction);
-                    boolean leavesSuccess = makeLeavesSlice(filler, iSeedReader, leavesPos, 1);
+                BlockPos leavesPos = blockPos.offset(2, leavesHeight, 2).relative(DIRECTIONS[j]);
+                boolean leavesSuccess = makeLeavesSlice(leavesFiller, reader, leavesPos, 2);
 
-                    if (!leavesSuccess) {
-                        return false;
-                    }
-                }
-            }
-        }
-
-        fill(iSeedReader, filler, false);
-        fill(iSeedReader, leavesFiller, true);
-        return false;
-    }
-
-    public static boolean makeBranch(ArrayList<Entry> filler, ArrayList<Entry> leavesFiller, ISeedReader reader, BlockPos pos, int height) {
-        for (int k = 0; k < height; k++) {
-            BlockPos branchPos = pos.above(k);
-            if (!canPlace(reader, branchPos)) {
-                return false;
-            }
-            filler.add(new Entry(branchPos, STALK));
-            if (k == height - 1) {
-                for (Direction direction : DIRECTIONS) {
-                    BlockPos leavesPos =  branchPos.relative(direction);
-                    leavesFiller.add(new Entry(leavesPos, PADS));
-                }
-                leavesFiller.add(new Entry(branchPos.above(), PADS));
-
-            }
-        }
-        return true;
-    }
-
-    public static boolean makeSlice(ArrayList<Entry> filler, ISeedReader reader, BlockPos pos, int sliceSize) {
-        for (int x = -sliceSize; x <= sliceSize; x++) {
-            for (int z = -sliceSize; z <= sliceSize; z++) {
-                if (Math.abs(x) == sliceSize && Math.abs(z) == sliceSize) {
-                    continue;
-                }
-
-                BlockPos slicePos = new BlockPos(pos).offset(x, 0, z);
-                if (!canPlace(reader, slicePos)) {
+                if (!leavesSuccess) {
                     return false;
                 }
-                filler.add(new Entry(slicePos, STALK));
             }
         }
-        return true;
+
+        fill(reader, filler, false);
+        fill(reader, leavesFiller, true);
+        return false;
     }
 
     public static boolean makeLeavesSlice(ArrayList<Entry> filler, ISeedReader reader, BlockPos pos, int sliceSize) {
@@ -108,8 +69,10 @@ public class LaminaTreeFeature extends Feature<ProbabilityConfig> {
                 if (Math.abs(x) == sliceSize && Math.abs(z) == sliceSize) {
                     continue;
                 }
+                int xOffset = x % 2;
+                int zOffset = z / 2;
 
-                BlockPos slicePos = new BlockPos(pos).offset(x, 0, z);
+                BlockPos slicePos = new BlockPos(pos).offset(xOffset, 0, zOffset);
 
                 if (!canPlace(reader, slicePos)) {
                     return false;
