8412cc915c0b4019c3d0645e6cf0c5350335d6ca
Coda <cody.koelliker@outlook.com>
2022-02-12 01:58:46 -0700

    teal arrowfish renderer

---
+++ src/main/java/teamdraco/finsandstails/client/render/TealArrowfishRenderer.java
@@ -0,0 +1,15 @@
+
+package teamdraco.finsandstails.client.render;
+
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import software.bernie.finsandtails.geckolib3.renderers.geo.GeoProjectilesRenderer;
+import teamdraco.finsandstails.client.model.TealArrowfishModel;
+import teamdraco.finsandstails.common.entities.TealArrowfishEntity;
+
+public class TealArrowfishRenderer extends GeoProjectilesRenderer<TealArrowfishEntity> {
+
+    public TealArrowfishRenderer(EntityRendererProvider.Context context) {
+        super(context, new TealArrowfishModel());
+        this.shadowRadius = 0.2F;
+    }
+}
