72ec9cf651a70146407bc5c750468f137f538835
Coda <cody.koelliker@outlook.com>
2022-03-14 02:15:39 -0600

    more heart work..... i think. this is confusing

--- src/main/java/teamdraco/finsandstails/client/ClientEvents.java
+++ src/main/java/teamdraco/finsandstails/client/ClientEvents.java
@@ -2,7 +2,9 @@ package teamdraco.finsandstails.client;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.player.LocalPlayer;
@@ -10,11 +12,15 @@ import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.util.Mth;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.boss.enderdragon.phases.DragonSittingAttackingPhase;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodData;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
@@ -140,118 +146,143 @@ public class ClientEvents {
                 RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
 
                 for (int i = 0; i < 4; i++) {
-                    int row = (int) (Math.ceil(i) / 10f);
-                    int x = left + i % 10 * 8;
-                    int y = top - row * 3 + rowHeight * 2 - 5;
-
-                    int u = 0;
-                    int v = 0;
-
-                    // Some of these values are being assigned to what they already are, but I'm keeping it because otherwise it annoys me :)
-                    if (chest.is(FTItems.SPINDLY_RUBY_CHARM.get())) {
-                        if (i * 2 > player.getHealth()) {
-                            // half heart
-                            u = 9;
-                            v = 0;
-                        } else {
-                            // full heart
-                            u = 0;
-                            v = 0;
-                        }
-                    }
-                    if (chest.is(FTItems.SPINDLY_EMERALD_CHARM.get())) {
-                        if (i * 2 > player.getHealth()) {
-                            u = 9;
-                            v = 9;
-                        } else {
-                            u = 0;
-                            v = 9;
-                        }
-                    }
-                    if (chest.is(FTItems.SPINDLY_AMBER_CHARM.get())) {
-                        if (i * 2 > player.getHealth()) {
-                            u = 9;
-                            v = 18;
-                        } else {
-                            u = 0;
-                            v = 18;
-                        }
-                    }
-                    if (chest.is(FTItems.SPINDLY_PEARL_CHARM.get())) {
-                        if (i * 2 > player.getHealth()) {
-                            u = 9;
-                            v = 27;
-                        } else {
-                            u = 0;
-                            v = 27;
-                        }
-                    }
-                    if (chest.is(FTItems.SPINDLY_SAPPHIRE_CHARM.get())) {
-                        if (i * 2 > player.getHealth()) {
-                            u = 9;
-                            v = 36;
-                        } else {
-                            u = 0;
-                            v = 36;
-                        }
-                    }
-
-                    double health0 = Math.ceil(player.getHealth());
-
-                    if (health0 <= 8) {
-                        // 1 heart
-                        if (health0 >= 2 && i == 0) {
-
-                            Screen.blit(poseStack, x, y + 3, u, v, 9, 9, 256, 256);
-                        }
-                       else if (health0 >= 1 && health0 < 2 && i == 0) {
-                            Screen.blit(poseStack, x, y + 3, u + 9, v, 9, 9, 256, 256);
-                       }
-                       else if (health0 < 1 && i == 0) {
-                            Screen.blit(poseStack, x, y + 3, u + 18, v, 9, 9, 256, 256);
-                        }
-
-                        // 2 hearts
-                        if (health0 >= 4 && i == 1) {
-                            Screen.blit(poseStack, x, y + 3, u, v, 9, 9, 256, 256);
-                        }
-                        else if (health0 >= 3 && health0 < 4 && i == 1) {
-                            Screen.blit(poseStack, x, y + 3, u + 9, v, 9, 9, 256, 256);
-                        }
-                        else if (health0 < 3 && i == 1) {
-                            Screen.blit(poseStack, x, y + 3, u + 18, v, 9, 9, 256, 256);
-                        }
-
-                        // 3 hearts
-                        if (health0 >= 6 && i == 2) {
-                            Screen.blit(poseStack, x, y + 3, u, v, 9, 9, 256, 256);
-                        }
-                        else if (health0 >= 5 && health0 < 6 && i == 2) {
-                            Screen.blit(poseStack, x, y + 3, u + 9, v, 9, 9, 256, 256);
-                        }
-                        else if (health0 < 5 && i == 2) {
-                            Screen.blit(poseStack, x, y + 3, u + 18, v, 9, 9, 256, 256);
-                        }
-
-                        // 4 hearts
-                        if (health0 >= 8 && i == 3) {
-                            Screen.blit(poseStack, x, y + 3, u, v, 9, 9, 256, 256);
-                        }
-                        else if (health0 >= 7 && health0 < 8 && i == 3) {
-                            Screen.blit(poseStack, x, y + 3, u + 9, v, 9, 9, 256, 256);
-                        }
-                        else if (health0 < 7 && i == 3) {
-                            Screen.blit(poseStack, x, y + 3, u + 18, v, 9, 9, 256, 256);
-                        }
-                    }
-                    else {
-                        Screen.blit(poseStack, x, y + 3, u, v, 9, 9, 256, 256);
-                    }
-
+                    renderPlayerHealth(poseStack, player);
                 }
                 RenderSystem.depthMask(true);
                 RenderSystem.disableBlend();
                 poseStack.popPose();
+            }
+        }
+
+        private static void renderPlayerHealth(PoseStack stack, Player player) {
+            Gui gui = Minecraft.getInstance().gui;
+
+            if (player != null) {
+                int i = Mth.ceil(player.getHealth());
+                boolean flag = gui.healthBlinkTime > (long)gui.tickCount && (gui.healthBlinkTime - (long)gui.tickCount) / 3L % 2L == 1L;
+                long j = Util.getMillis();
+                if (i < gui.lastHealth && player.invulnerableTime > 0) {
+                    gui.lastHealthTime = j;
+                    gui.healthBlinkTime = (gui.tickCount + 20);
+                } else if (i > gui.lastHealth && player.invulnerableTime > 0) {
+                    gui.lastHealthTime = j;
+                    gui.healthBlinkTime = gui.tickCount + 10;
+                }
+
+                if (j - gui.lastHealthTime > 1000L) {
+                    gui.lastHealth = i;
+                    gui.displayHealth = i;
+                    gui.lastHealthTime = j;
+                }
+
+                gui.lastHealth = i;
+                int k = gui.displayHealth;
+                player.getRandom().setSeed((gui.tickCount * 312871L));
+                int i1 = gui.screenWidth / 2 - 91;
+                int k1 = gui.screenHeight - 39;
+                float f = Math.max((float)player.getAttributeValue(Attributes.MAX_HEALTH), (float)Math.max(k, i));
+                int l1 = Mth.ceil(player.getAbsorptionAmount());
+                int i2 = Mth.ceil((f + (float)l1) / 2.0F / 10.0F);
+                int j2 = Math.max(10 - (i2 - 2), 3);
+                int k2 = k1 - (i2 - 1);
+                int j3 = -1;
+                if (player.hasEffect(MobEffects.REGENERATION)) {
+                    j3 = gui.tickCount % Mth.ceil(f + 5.0F);
+                }
+
+                for(int k3 = 0; k3 < 4; ++k3) {
+                    int l3 = i1 + k3 * 8;
+                    gui.blit(stack, l3, k2, 34, 9, 9, 9);
+
+                    // last 2 unneeded?
+                    gui.blit(stack, l3, k2, 25, 9, 9, 9);
+
+                    gui.blit(stack, l3, k2, 16, 9, 9, 9);
+                }
+
+                gui.minecraft.getProfiler().popPush("health");
+                renderHearts(stack, player, i1, k1, j2, j3, f, i, k, l1, flag);
+            }
+        }
+
+        public static void renderHearts(PoseStack stack, Player player, int p_168691_, int p_168692_, int p_168693_, int p_168694_, float p_168695_, int p_168696_, int p_168697_, int p_168698_, boolean p_168699_) {
+            CharmType charm = CharmType.getCharm(player);
+            int i = 9 * (player.level.getLevelData().isHardcore() ? 5 : 0);
+            int j = Mth.ceil((double) p_168695_ / 2.0D);
+            int k = Mth.ceil((double) p_168698_ / 2.0D);
+
+            for (int i1 = j + k - 1; i1 >= 0; --i1) {
+                int j1 = i1 / 10;
+                int k1 = i1 % 2;
+                int l1 = p_168691_ + k1 * 8;
+                int i2 = p_168692_ - j1 * p_168693_;
+                if (p_168696_ + p_168698_ <= 4) {
+                    i2 += player.getRandom().nextInt(2);
+                }
+
+                if (i1 < j && i1 == p_168694_) {
+                    i2 -= 2;
+                }
+
+                int j2 = i1 * 2;
+
+                if (p_168699_ && j2 < p_168697_) {
+                    boolean flag2 = j2 + 1 == p_168697_;
+                    renderHeart(stack, charm, l1, i2, i, true, flag2);
+                }
+            }
+        }
+
+        public static void renderHeart(PoseStack p_168701_, CharmType type, int p_168703_, int p_168704_, int p_168705_, boolean p_168706_, boolean p_168707_) {
+            Minecraft.getInstance().gui.blit(p_168701_, p_168703_, p_168704_, type.getX(p_168707_, p_168706_), p_168705_, 9, 9);
+        }
+
+        public enum CharmType {
+            RUBY(2),
+            EMERALD(4),
+            AMBER(6),
+            PEARL(8),
+            SAPPHIRE(10),
+            NORMAL(12);
+
+            private final int index;
+
+            CharmType(int p_168729_) {
+                this.index = p_168729_;
+            }
+
+            public int getX(boolean p_168735_, boolean p_168736_) {
+                int j = p_168735_ ? 1 : 0;
+                int k = p_168736_ ? 2 : 0;
+                int i = j + k;
+
+                return (index * 2 + i) * 9;
+            }
+
+            static CharmType getCharm(Player p_168733_) {
+                ItemStack stack = p_168733_.getItemBySlot(EquipmentSlot.CHEST);
+                CharmType type;
+
+                if (stack.is(FTItems.SPINDLY_RUBY_CHARM.get())) {
+                    type = RUBY;
+                }
+                if (stack.is(FTItems.SPINDLY_AMBER_CHARM.get())) {
+                    type = EMERALD;
+                }
+                if (stack.is(FTItems.SPINDLY_AMBER_CHARM.get())) {
+                    type = AMBER;
+                }
+                if (stack.is(FTItems.SPINDLY_PEARL_CHARM.get())) {
+                    type = PEARL;
+                }
+                if (stack.is(FTItems.SPINDLY_SAPPHIRE_CHARM.get())) {
+                    type = SAPPHIRE;
+                }
+                else {
+                    type = NORMAL;
+                }
+
+                return type;
             }
         }
     }
