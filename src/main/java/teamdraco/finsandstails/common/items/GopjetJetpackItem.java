91d36b3df07beeccdf8615e9fa9049b051c64aaf
baguchan <shutux500@gmail.com>
2023-03-14 11:00:41 +0900

    fixed gobjet pack movement

--- src/main/java/teamdraco/finsandstails/common/items/GopjetJetpackItem.java
+++ src/main/java/teamdraco/finsandstails/common/items/GopjetJetpackItem.java
@@ -99,7 +99,7 @@ public class GopjetJetpackItem extends GeoArmorItem implements IAnimatable {
                             stack.hurtAndBreak(1, player, Player -> Player.broadcastBreakEvent(EquipmentSlot.CHEST));
                         }
                         persistentData.putInt("FinsFlyingTicks", ticksJumping);
-                        Vec3 d3 = player.getViewVector(1.0F);
+                        Vec3 d3 = player.getViewVector(1.0F).scale(0.35F);
                         if (!player.isOnGround()) {
                             Vec3 vec31 = Vec3.ZERO;
                             player.setDeltaMovement(vec31.add(d3));
