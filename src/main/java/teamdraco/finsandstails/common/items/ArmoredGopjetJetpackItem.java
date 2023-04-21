68a039a829239fabc1d6c0d1d8e8953abe5312f9
ItsBlackGear <itsblackgear@gmail.com>
2023-04-21 16:51:52 -0400

    parity with 1.18.2 from baguchan's last PR

--- src/main/java/teamdraco/finsandstails/common/items/ArmoredGopjetJetpackItem.java
+++ src/main/java/teamdraco/finsandstails/common/items/ArmoredGopjetJetpackItem.java
@@ -101,7 +101,7 @@ public class ArmoredGopjetJetpackItem extends GeoArmorItem implements IAnimatabl
                             stack.hurtAndBreak(1, player, Player -> Player.broadcastBreakEvent(EquipmentSlot.CHEST));
                         }
                         persistentData.putInt("FinsFlyingTicks", ticksJumping);
-                        Vec3 d3 = player.getViewVector(1.0F);
+                        Vec3 d3 = player.getViewVector(1.0F).scale(0.5F);
                         if (!player.isOnGround()) {
                             Vec3 vec31 = Vec3.ZERO;
                             player.setDeltaMovement(vec31.add(d3));
